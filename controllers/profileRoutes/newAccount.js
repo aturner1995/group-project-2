d97e44033058f30959a2b82c3d57b1d1cb9f755d
Renjithkumar993 <121647888+Renjithkumar993@users.noreply.github.com>
2023-04-26 19:07:23 -0300

    added module.exports on modal index

--- controllers/profileRoutes/newAccount.js
+++ controllers/profileRoutes/newAccount.js
@@ -1,11 +1,12 @@
 
-const {Person,Work,Education,Certification,Overview ,Skill} = require("../../models")
+const {Person,Work,Education,Certification,Overview ,Skill} = require("../../models");
+const Project = require("../../models/project");
 const router = require("express").Router();
 
 router.get("/",async (req, res) => {
 
   const dbData  = await Person.findByPk(1,{
-    include :[ {model:Work}]
+    include :[ {model:Work}, {model : Education},{model : Project},{model : Certification}]
   })
 
   const test = dbData.get({ plain: true });
