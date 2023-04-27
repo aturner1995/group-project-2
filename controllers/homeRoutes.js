c1c192cb802a3f8bddd27346edfdcc77bc3afe56
Beezyz <116973964+Beezyz@users.noreply.github.com>
2023-04-27 15:34:16 -0300

    add login.js and revise login.handlebars with path to login.js

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,9 +1,13 @@
 const router = require('express').Router();
-
+const { Project, User } = require('../models');
+const withAuth = require('../utils/auth');
 
 router.get('/', (req, res) => {
 
     res.render('homepage')
 });
+
+
+
 
 module.exports = router;
