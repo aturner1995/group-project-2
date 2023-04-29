574276668d2e565b403595818ef144c7e4c21ccf
Aaron <aaturner1995@gmail.com>
2023-04-29 13:06:25 -0300

    added generate resume form with multipl colours and forms

--- public/js/resume.js
+++ public/js/resume.js
@@ -1,40 +1,50 @@
 const generateResume = async (event) => {
-    const response = await fetch('api/resume/generate', {
+    event.preventDefault();
+    // Retrieve the selected color value
+    const selectedColor = document.querySelector('input[name="color"]:checked').value;
+
+    // Retrieve the selected template value
+    const template = document.querySelector('input[name="template"]:checked').value;
+    let selectedTemplate;
+    if (template === 'template1') {
+        selectedTemplate = 1;
+    }
+    else {
+        selectedTemplate = 2;
+    }
+
+    // Do something with the selected values (e.g., send an AJAX request to the server)
+    console.log(selectedColor, selectedTemplate);
+
+    const response = await fetch(`api/resume/generate/${selectedTemplate}?color=${selectedColor}`, {
         method: 'GET',
-        headers: {'Content-Type': 'application/pdf'}
+        headers: { 'Content-Type': 'application/pdf' }
     })
 
     if (response.ok) {
-        const viewResponse = await fetch('api/resume/view', {
-            method: 'GET',
-            headers: {'Content-Type': 'application/pdf'}
-        })
-
-        if (viewResponse.ok) {
-            document.location.reload();
-        }
-        else {
-            alert('Failed to view Resume');
-        }
+        document.location = '/resume';
     }
     else {
         alert('Failed to create Resume');
     }
 };
 
-const downloadResume = async (event) => {
-    const response = await fetch('/api/resume/download', {
-        method: 'GET',
-        headers: {'Content-Type': 'application/pdf'}
-    })
+// const downloadResume = async (event) => {
+//     const response = await fetch('/api/resume/download', {
+//         method: 'GET',
+//         headers: { 'Content-Type': 'application/pdf' }
+//     })
 
-    if (response.ok) {
-        document.location.reload();
-    }
-    else {
-        alert('Failed to view Resume');
-    }
-};
+//     if (response.ok) {
+//         document.location.reload();
+//     }
+//     else {
+//         alert('Failed to view Resume');
+//     }
+// };
 
-document.querySelector('.view-resume').addEventListener('click', generateResume);
-document.querySelector('.download').addEventListener('click', downloadResume);
+// document.querySelector('.view-resume').addEventListener('click', generateResume);
+// document.querySelector('.download').addEventListener('click', downloadResume);
+
+// Select the form element
+const form = document.querySelector('.card').addEventListener('submit', generateResume)
