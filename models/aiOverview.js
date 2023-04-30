27feecd1a25548cd293402fdfaff2f81a500f5c3
Aaron <aaturner1995@gmail.com>
2023-04-30 18:49:52 -0300

    added ai generated resume for updating the overview. Will add job experience tomorrow

---
+++ models/aiOverview.js
@@ -0,0 +1,34 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class AiOverview extends Model { }
+
+AiOverview.init({
+
+  id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  text: {
+    type: DataTypes.TEXT,
+    allowNull: false,
+  },
+  user_id: {
+    type: DataTypes.INTEGER,
+    references: {
+      model: 'user',
+      key: 'id'
+    }
+  }
+},
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    modelName: 'aioverview',
+    underscored: true,
+  });
+
+module.exports = AiOverview;
--- public/js/resume.js
+++ public/js/resume.js
@@ -13,9 +13,6 @@ const generateResume = async (event) => {
         selectedTemplate = 2;
     }
 
-    // Do something with the selected values (e.g., send an AJAX request to the server)
-    console.log(selectedColor, selectedTemplate);
-
     const response = await fetch(`api/resume/generate/${selectedTemplate}?color=${selectedColor}`, {
         method: 'GET',
         headers: { 'Content-Type': 'application/pdf' }
@@ -29,22 +26,4 @@ const generateResume = async (event) => {
     }
 };
 
-// const downloadResume = async (event) => {
-//     const response = await fetch('/api/resume/download', {
-//         method: 'GET',
-//         headers: { 'Content-Type': 'application/pdf' }
-//     })
-
-//     if (response.ok) {
-//         document.location.reload();
-//     }
-//     else {
-//         alert('Failed to view Resume');
-//     }
-// };
-
-// document.querySelector('.view-resume').addEventListener('click', generateResume);
-// document.querySelector('.download').addEventListener('click', downloadResume);
-
-// Select the form element
-const form = document.querySelector('.card').addEventListener('submit', generateResume)
+document.querySelector('.gen-resume').addEventListener('click', generateResume)
